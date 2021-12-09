aba824f2d64f9899c04a3ed6e5c4933a54e249ac
A7t5i0 <84842351+A7t5i0@users.noreply.github.com>
2021-12-09 08:51:23 +0200

    Update mg.rb

--- mg.rb
+++ mg.rb
@@ -2,7 +2,7 @@ def game
   gamecommands = ['sudo pacman -Syyu', 'sudo pacman -S yay', 'yay -S nvidia lutris mesa optimus-manager optimus-manager-qt steam game-mode', 'pip3 install protonup']
   for gamecommand in gamecommands
     puts '>>>COMMAND IN PROGRESS:' + gamecommand
-    exec gamecommand
+    system gamecommand
     puts '>>>COMMAND COMPLETE!'
   end
 end
